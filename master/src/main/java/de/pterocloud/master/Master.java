d2b3392e2d939ef839dfac131d4d1c62f04016be
TheCurano <thecurano@outlook.de>
2022-10-03 16:12:36 +0200

    Upload first files

---
+++ master/src/main/java/de/pterocloud/master/Master.java
@@ -0,0 +1,23 @@
+package de.pterocloud.master;
+
+import org.jline.reader.Completer;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.terminal.Terminal;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class Master {
+
+    public static void inputReader(Path historyFile, Completer... completers)
+            throws IOException
+    {
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
